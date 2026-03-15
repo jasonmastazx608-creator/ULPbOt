64ed85cb8741fccb196238aa32ce9bafc1bcc4e5
jasonmastazx608-creator <jasonmastazx608@gmail.com>
2026-03-15 12:16:03 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 #Copyright @ISmartCoder
 #Updates Channel @abirxdhackz
-API_ID = YOUR_API_ID
-API_HASH = 'YOUR_API_HASH''
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
-UPDATE_CHANNEL_URL = 't.me/abirxdhackz'
+API_ID = bbad3gAenTY
+API_HASH = '81f364fab45d02a750d8ffe0f1ab651b'
+BOT_TOKEN = '8531807619:AAGmkwxfszumLOsH6CCjgngq-OUVzGiRKO4'
+UPDATE_CHANNEL_URL = 'https://t.me/LET_S_DO_IT'
 COMMAND_PREFIXES = ['/', '!', '.', ',', '$', '#']
 
-OWNER_ID = 7666341631
-ADMIN_ID = 7666341631
+OWNER_ID = 7213964175
+ADMIN_ID = 7213964175
